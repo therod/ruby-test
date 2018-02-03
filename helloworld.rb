1d98a99363ff4dbf2139fdec38c989bb5c9d2d28
Rodrigo Haenggi <rodrigo@codegestalt.com>
2018-02-03 16:07:24 +0100

    Add first program to the repository

---
+++ helloworld.rb
@@ -0,0 +1,10 @@
+print "link: "
+link = gets.chomp
+print "searchterm: "
+term = gets.chomp
+
+system "curl #{link} | html2text | grep #{term}"
+
+puts "Thanks for using this awesome application!"
+
+puts "another change."
